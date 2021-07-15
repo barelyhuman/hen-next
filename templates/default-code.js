8fe17ca90e9f5068b074142229481dd03ccdff92
Siddharth <ahoy@barelyhuman.dev>
2021-07-15 09:08:02 +0530

    docs: add example for hover style
    added example for hovering style into the default code

--- templates/default-code.js
+++ templates/default-code.js
@@ -1,5 +1,6 @@
 export default `// You can import modules from esm and use them accordingly
 import cn from "https://esm.sh/clsx";
+import {If} from "https://esm.sh/react-extras";
 import {format} from "https://esm.sh/@barelyhuman/date-utils";
 
 () => {
@@ -7,7 +8,9 @@ import {format} from "https://esm.sh/@barelyhuman/date-utils";
   const classList = cn("bold");
   return (
     <>
-      <button>{format("hh:mm",new Date())}</button>
+      <If condition={Math.random()>0.3}>
+        <button>{format("hh:mm",new Date())}</button>
+      </If>
       <style jsx>{\`
         @import url("https://unpkg.com/open-color@1.8.0/open-color.css");
 
@@ -21,6 +24,11 @@ import {format} from "https://esm.sh/@barelyhuman/date-utils";
           color: var(--oc-gray-1);
           padding: 6px 12px;
           border-radius: 4px;
+        }
+        
+        button:hover{
+          background: var(--oc-gray-7);
+          cursor:pointer;
         }
       \`}</style>
     </>
